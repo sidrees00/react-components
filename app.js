ecfe3d6fb3ae018a5db15cbc40fb14c97fdb2f9d
Suhail Idrees <gusmanbhai@hotmail.com>
2016-11-20 15:08:58 -0800

    Add click event to grocery list

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-var GroceryListItem = (props) => (
-	<ul>
-		<li>{props.list[0]}</li>
+var GroceryListItem = (props) => {
+	
+	//function that will be invoked in response to event
+	var clickHandler = () => (
+		console.log('Hello World!!!')
+		);
+
+	// Since you are using {} after =>, you must include an explicit return
+	// onClick = {function where you want to be directed}
+	return (<ul>
+		<li onClick={clickHandler}>{props.list[0]}</li>
 		<li>{props.list[1]}</li>
 		<li>{props.list[2]}</li>
-	</ul>
+	</ul>);
 
-	);
+};
 
-//Grocery list is rendered
-// Below, you are rendering an instance of GroceryListItem while passing the object list as a paramter. 
 var GroceryList = () => (
 	<div>
 		<h2>My Grocery List:</h2>
